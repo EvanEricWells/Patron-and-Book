a2a96bf0be96485f184eb43260d69080a6e8e6a1
evan5090 <evan5090@ISS147D8-19.scdsb.pri>
2014-11-04 09:12:02 -0500

    ayy lmao

---
+++ src/Book.java
@@ -0,0 +1,22 @@
+public class Book {
+
+    private String Title = "";
+    private String Author = "";
+
+    public Book(String a, String b) {
+        Title += a;
+        Author += b;
+    }
+
+    public String toString() {
+        String result = "";
+        result += Title;
+        result += Author;
+        return result;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+}
---
+++ src/Patron.java
@@ -0,0 +1,73 @@
+public class Patron {
+
+    private String Name;
+    Book b1;
+    Book b2;
+    Book b3;
+
+    public Patron(String n) {
+        Name = n;
+        b1 = null;
+        b2 = null;
+        b3 = null;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public boolean canBorrow() {
+        if (b1 == null) {
+            return true;
+        } else if (b2 == null) {
+            return true;
+        } else if (b3 == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean borrow(Book b) {
+        if (b1 == null) {
+            b1 = b;
+            return true;
+        } else if (b2 == null) {
+            b2 = b;
+            return true;
+        } else if (b3 == null) {
+            b3 = b;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean returnBook(Book b) {
+        if (b == b1) {
+            b1 = null;
+            return true;
+        } else if (b == b2) {
+            b2 = null;
+            return true;
+        } else if (b == b3) {
+            b3 = null;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean hasBook(Book b) {
+        if (b == b1) {
+            return true;
+        } else if (b == b2) {
+            return true;
+        } else if (b == b3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
